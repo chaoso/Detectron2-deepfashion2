fd5cb1e905f386d1968a994e17705133d0750ea1
Harry Vuong <harry_95_v@hotmail.com>
2019-11-13 16:07:07 +0100

    Clean up extract file script

--- deepfashion2/extract_subset_of_files.py
+++ deepfashion2/extract_subset_of_files.py
@@ -16,25 +16,20 @@ DST_TRAINING_IMAGE_FILE_PATH = "F:\\mini_deepfashion2\\train\\image"
 i = 0
 for path, _, files in os.walk(TRAINING_IMAGE_FILE_PATH):
     for file in files:
-        if i == 100:
+        if i == NUMBER_OF_SAMPLES_TRAIN:
             break
-        print(os.path.join(path, file))
         shutil.copy(os.path.join(path, file), DST_TRAINING_IMAGE_FILE_PATH)
         i += 1
     break
 
 j = 0
 for path, _, files in os.walk(VALIDATION_IMAGE_FILE_PATH):
-    print(path)
     for file in files:
-        if j == 100:
+        if j == NUMBER_OF_SAMPLES_VALIDATION:
             break
-        print(os.path.join(path, file))
         shutil.copy(os.path.join(path, file), DST_VALIDATION_IMAGE_FILE_PATH)
         j += 1
     break
 
-# copyfile(TRAINING_IMAGE_FILE_PATH, DST_TRAINING_IMAGE_FILE_PATH)
-# copyfile(DST_VALIDATION_IMAGE_FILE_PATH, DST_VALIDATION_IMAGE_FILE_PATH)
 
 print('Done (t={:0.2f}s)'.format(time.time() - tic))
